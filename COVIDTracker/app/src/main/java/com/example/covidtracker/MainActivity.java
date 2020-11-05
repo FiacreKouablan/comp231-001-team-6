7a1c73e56d4b72bfa7d29b16b70e71c92774213f
TBriggz <skaterbriggz@gmail.com>
2020-11-05 01:55:50 -0500

    Added login activity
    Basic login functionality.

--- COVIDTracker/app/src/main/java/com/example/covidtracker/MainActivity.java
+++ COVIDTracker/app/src/main/java/com/example/covidtracker/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.covidtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            //button to open login page.
+            case R.id.loginBtn:
+
+                setContentView(R.layout.activity_login);
+                Toast.makeText(this, "Welcome back to Covid Tracer", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(view.getContext(), Login.class));
+                break;
+
+            //Placeholder code for second button.
+//            case R.id.signupBtn:
+//
+//                //setContentView(R.layout.activity_exercise1);
+//                Toast.makeText(this, "Welcome to Covid Tracer", Toast.LENGTH_LONG).show();
+//                startActivity(new Intent(view.getContext(), Register.class));
+//                break;
+
+            default: setContentView(R.layout.activity_main);
+        }
+
     }
 }
