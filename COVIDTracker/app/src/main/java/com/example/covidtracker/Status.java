cab3ccdd8c416a31b463dd8da4767b7874b89be5
JoshuaTome <74151345+JoshuaTome@users.noreply.github.com>
2020-11-08 17:34:55 -0500

    Added Status and Update Status Pages

--- COVIDTracker/app/src/main/java/com/example/covidtracker/Status.java
+++ COVIDTracker/app/src/main/java/com/example/covidtracker/Status.java
@@ -1,22 +1,16 @@
 package com.example.covidtracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Status extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
-    public void onClick(View view)
-    {
         setContentView(R.layout.activity_status);
-        startActivity(new Intent(view.getContext(), Status.class));
     }
 }
