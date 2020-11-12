e3c64aee6bf5674f1e4011c1a095c4d7b77458c3
FiacreKouablan <48250714+FiacreKouablan@users.noreply.github.com>
2020-11-11 19:26:13 -0500

    first modification

---
+++ COVIDTracker/app/src/main/java/com/example/covidtracker/ExposedPatient.java
@@ -0,0 +1,14 @@
+package com.example.covidtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExposedPatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exposed_patient);
+    }
+}
